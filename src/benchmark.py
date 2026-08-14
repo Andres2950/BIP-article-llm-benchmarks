a2555bb4b4591b1b9d6775b8a94bc80a68985db4
Oscar Obando <andres.umana777@gmail.com>
2026-08-14 15:03:44 -0600

    feat: reading dataset is ready

--- src/benchmark.py
+++ src/benchmark.py
@@ -6,12 +6,11 @@ from resource_monitor import ResourceMonitor, find_ollama_pid
 from utils import build_prompt
 
 class Benchmark:
-    def __init__(self, model, temperature, context, question, show_response=False):
+    def __init__(self, model, temperature, context, question):
         self.model = model
         self.temperature = temperature
         self.context = context
         self.question = question
-        self.show_response = show_response
         self.llm = ChatOllama(model=model, temperature=temperature)
         self.prompt = build_prompt(context, question)
 
@@ -26,46 +25,47 @@ class Benchmark:
         response = self.llm.invoke(self.prompt)
         monitor.stop()
 
+        resources = monitor.get_resource_usage()
+
         result = {
+            "model": self.model,
             "response": response.content,
-            "model": response.response_metadata["model"],
             "total_duration": response.response_metadata["total_duration"],
             "load_duration": response.response_metadata["load_duration"],
             "input_tokens": response.usage_metadata["input_tokens"],
             "output_tokens": response.usage_metadata["output_tokens"],
             "total_tokens": response.usage_metadata["total_tokens"],
-            "resources": monitor.get_resource_usage()
+            "cpu": resources["cpu"],
+            "memory": resources["memory"],
+            "gpu_util": resources["gpu_util"],
+            "gpu_mem": resources["gpu_mem"]
         }
 
         return result
     
-    def print_question_result(self):
-        result = self.run_question()
+    def print_question_result(self, result):
         cpu_cores = psutil.cpu_count(logical=False)
-
-        if self.show_response:
-            print("\n" + "=" * 10)
-            print("Respuesta:\n")
-            print(result["response"])
 
         print("\n" + "=" * 10)
         print("Modelo: ", result["model"])
+        print("Respuesta: ", result["response"])
+
+        print("\n" + "=" * 10)
         print("Duración total: ", result["total_duration"])
         print("Duración de carga: ", result["load_duration"])
         print("Tokens de input: ", result["input_tokens"])
         print("Tokens de output: ", result["output_tokens"])
         print("Tokens totales: ", result["total_tokens"])
 
-        if "resources" in result:
-            print("\n" + "=" * 10)
-            print("Recursos:")
-            if result["resources"]["cpu"] is not None:
-                print(f"CPU: {result['resources']['cpu']['avg']}% (min: {result['resources']['cpu']['min']}%, max: {result['resources']['cpu']['max']}%)")
-                print(f"CPU por núcleo ({cpu_cores} núcleos): {result['resources']['cpu']['avg'] / cpu_cores}% (min: {result['resources']['cpu']['min'] / cpu_cores}%, max: {result['resources']['cpu']['max'] / cpu_cores}%)")
-            if result["resources"]["memory"] is not None:
-                print(f"RAM: {result['resources']['memory']['avg']}MB (min: {result['resources']['memory']['min']}MB, max: {result['resources']['memory']['max']}MB)")
-            if result["resources"]["gpu_util"] is not None:
-                print(f"GPU: {result['resources']['gpu_util']['avg']}% (min: {result['resources']['gpu_util']['min']}%, max: {result['resources']['gpu_util']['max']}%)")
-            if result["resources"]["gpu_mem"] is not None:
-                print(f"GPU Mem: {result['resources']['gpu_mem']['avg']}MB (min: {result['resources']['gpu_mem']['min']}MB, max: {result['resources']['gpu_mem']['max']}MB)")
+        print("\n" + "=" * 10)
+        print("Recursos:")
+        if result["cpu"] is not None:
+            print(f"CPU: {result['cpu']['avg']}% (min: {result['cpu']['min']}%, max: {result['cpu']['max']}%)")
+            print(f"CPU por núcleo ({cpu_cores} núcleos): {result['cpu']['avg'] / cpu_cores}% (min: {result['cpu']['min'] / cpu_cores}%, max: {result['cpu']['max'] / cpu_cores}%)")
+        if result["memory"] is not None:
+            print(f"RAM: {result['memory']['avg']}MB (min: {result['memory']['min']}MB, max: {result['memory']['max']}MB)")
+        if result["gpu_util"] is not None:
+            print(f"GPU: {result['gpu_util']['avg']}% (min: {result['gpu_util']['min']}%, max: {result['gpu_util']['max']}%)")
+        if result["gpu_mem"] is not None:
+            print(f"GPU Mem: {result['gpu_mem']['avg']}MB (min: {result['gpu_mem']['min']}MB, max: {result['gpu_mem']['max']}MB)")
         print("\n" + "=" * 10)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import argparse
 from pathlib import Path
+import pandas as pd
+import random
+from datetime import datetime
 
 from langchain_ollama import ChatOllama
 from langchain_community.document_loaders import PyPDFLoader
@@ -7,13 +10,29 @@ from langchain_community.document_loaders import PyPDFLoader
 from benchmark import Benchmark
 
 
+TYPES_RANGES = {
+    "yes_no": (1, 3),
+    "short_answer": (101, 103),
+    "open_ended": (201, 203)
+}
+
+BAG_SIZE_PER_TYPE = 3
+NUM_BAGS = 1
+
+# Función auxiliar (ponerla fuera del main)
+def get_question_type(q_id):
+    for tipo, (inicio, fin) in TYPES_RANGES.items():
+        if inicio <= q_id <= fin:
+            return tipo
+    return 'unknown'
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Models Benchmarking")
 
     parser.add_argument("--temperature", type=float, default=0.2, help="Temperature to use")
     parser.add_argument("--context-docs", type=str, default="./context_docs", help="Path to the context documents directory")
-    parser.add_argument("--question", type=str, default="Haz un resumen del contexto dado", help="Question to ask the model")
-    parser.add_argument("--show-response", action="store_true", help="Show the response of the model")
+    parser.add_argument("--dataset", type=str, default="./datasets/dataset.csv", help="Path to the CSV dataset file with columns: ID, Question, Answer")
+    parser.add_argument("--question_id", type=int, default=None, help="Question ID in the csv dataset file (if not provided, all questions will be benchmarked)")
 
     return parser.parse_args()
 
@@ -29,18 +48,91 @@ def load_context(dir):
     
     return "\n\n".join(context)
 
+def load_dataset(path, question_id=None):
+    df = pd.read_csv(path)
+    df["ID"] = df["ID"].astype(int)
+
+    if question_id is not None:
+        df = df[df["ID"] == question_id]
+        return df.to_dict("records")
+
+    groups = {}
+
+    for i, (start, end) in TYPES_RANGES.items():
+        group = df[(df["ID"] >= start) & (df["ID"] <= end)]
+        groups[i] = group.to_dict("records")
+
+    return groups
+    
+
 if __name__ == "__main__":
+    args = parse_arguments()
+    context = load_context(args.context_docs)
+    question_groups = load_dataset(args.dataset, args.question_id)
 
     models = [
         "qwen2.5-coder:3b",
         "llama3.2:3b"
     ]
 
-    args = parse_arguments()
-    context = load_context(args.context_docs)
+    if isinstance(question_groups, list):
+        print("Single question mode")
+        row = question_groups[0]
+        for model in models:
+            print(f"Benchmarking model {model} with question {row['ID']}")
+            question = row["Question"]
+            benchmark = Benchmark(model, args.temperature, context, question)
+            result = benchmark.run_question()
+            benchmark.print_question_result(result)
+        exit()
+
+    master_records = []
 
     for model in models:
-        print("#"*10 + f" MODEL: {model} " + "#"*10)
-        print("\n")
-        benchmark = Benchmark(model, args.temperature, context, args.question, args.show_response)
-        benchmark.print_question_result()
+        for bag_id in range(NUM_BAGS):
+            sampled_questions = []
+
+            for q_type, q_list in question_groups.items():
+                sample = random.choices(q_list, k=BAG_SIZE_PER_TYPE)
+                sampled_questions.extend(sample)
+
+            for row in sampled_questions:
+                question = row["Question"]
+                expected_answer = row["Answer"]
+                question_id = row["ID"]
+
+                benchmark = Benchmark(model, args.temperature, context, question)
+
+                result = benchmark.run_question()
+                
+                record = {
+                    "bag_id": bag_id,
+                    "model": model,
+                    "question_id": question_id,
+                    "expected_answer": expected_answer,
+                    "response": result["response"],
+                    "total_duration_sec": result["total_duration"] / 1e9,
+                    "load_duration_sec": result["load_duration"] / 1e9,
+                    "input_tokens": result["input_tokens"],
+                    "output_tokens": result["output_tokens"],
+                    "total_tokens": result["total_tokens"],
+                    "question_type": get_question_type(question_id)
+                }
+
+                for resource in ['cpu', 'memory', 'gpu_util', 'gpu_mem']:
+                    if result.get(resource) is not None:
+                        record[f"{resource}_avg"] = result[resource]["avg"]
+                        record[f"{resource}_min"] = result[resource]["min"]
+                        record[f"{resource}_max"] = result[resource]["max"]
+                    else:
+                        record[f"{resource}_avg"] = None
+                        record[f"{resource}_min"] = None
+                        record[f"{resource}_max"] = None
+
+                master_records.append(record)
+
+    df_raw_data = pd.DataFrame(master_records)
+    csv_path = f"benchmark_raw_data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+    df_raw_data.to_csv(csv_path, index=False)
+    print(f"Raw data saved to {csv_path}")
+    
